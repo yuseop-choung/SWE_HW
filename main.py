e82949559ac41d5cbcdb3e2ae570e28cb8c51b6b
YuSeop Choung <yscyh@naver.com>
2022-03-29 14:56:38 +0900

    print 2*number

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello World!")
 print("My Name is Yuseop Choung")
 for i in range(10):
-    print("number print:" + str(i + 1))
+    print("구구단 2단:" + str(i + 1) + "* 2 = " + str((i + 1) * 2))
